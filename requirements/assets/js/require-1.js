334809319e4b6c2bf36cd1bc2e4a64e635b0ec6b
thomascleverly <99171103+thomascleverly@users.noreply.github.com>
2022-03-19 02:40:12 +0700

    Tuan Anh code require 1. Done

---
+++ requirements/assets/js/require-1.js
@@ -0,0 +1,36 @@
+var createArrBtn = document.querySelector('.create-array-btn')
+var valuesArr = document.querySelector('.values-arr')
+createArrBtn.addEventListener('click', function(){
+    var length = Math.floor((Math.random()*11)+10)
+    var arrNumbers = []
+    for (var i=0; i<length; i++){
+        arrNumbers[i] = Math.floor((Math.random()*91)+10)
+    }
+    valuesArr.textContent = arrNumbers.join(', ')
+})
+
+var addBtn = document.querySelector('#add-btn')
+var valuesAfterAdd = document.querySelector('.values-after-add')
+addBtn.addEventListener('click', function(){
+    var firstArr = valuesArr.textContent.split(',')
+    var checkSortUp = document.querySelector('#sort-up').checked 
+    var checkSortDown = document.querySelector('#sort-down').checked 
+    var numberPosition = document.querySelector('#number-position').value
+    var valueAdd = document.querySelector('#value-add').value
+    if (checkSortUp){
+        var sortedArr = firstArr.sort()
+    }
+    else if (checkSortDown){
+        var sortedArr = firstArr.sort().reverse()
+    }
+    else{
+        var sortedArr = firstArr
+    }
+    sortedArr.splice(numberPosition-1,0,valueAdd)
+    // Cách 2:
+    // for (var i=firstArr.length; i>=numberPosition.value; i--){
+    //     firstArr[i] = firstArr[i-1]
+    // }
+    // firstArr[numberPosition.value-1] = valueAdd.value
+    valuesAfterAdd.textContent = sortedArr
+})
